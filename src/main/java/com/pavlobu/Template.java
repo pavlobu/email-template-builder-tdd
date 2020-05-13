4fe54e33d57aeeb223c3683910609b25c8429642
Pavlo Buidenkov <pavlobu@gmail.com>
2020-05-14 01:22:51 +0300

    refactoring more

--- src/main/java/com/pavlobu/Template.java
+++ src/main/java/com/pavlobu/Template.java
@@ -26,6 +26,10 @@ public class Template {
     public String evaluate() {
         TemplateParse parser = new TemplateParse();
         List<String> segments = parser.parse(templateText);
+        return concatenate(segments);
+    }
+
+    private String concatenate(List<String> segments) {
         StringBuilder result = new StringBuilder();
         for (String segment : segments) {
             append(segment, result);
@@ -33,12 +37,32 @@ public class Template {
         return result.toString();
     }
 
-    private void append(String segment, StringBuilder result) { if (segment.startsWith("${") && segment.endsWith("}")) {
-        String var = segment.substring(2, segment.length() - 1); if (!variables.containsKey(var)) {
+//    private String concatenate(List<Segment> segments) {
+//        StringBuilder result = new StringBuilder();
+//        for (Segment segment : segments) {
+//            segment.appendTo(result, variables);
+//        }
+//        return result.toString();
+//    }
+
+    private void append(String segment, StringBuilder result) {
+        if (isVariable(segment)) {
+            evaluateVariable(segment, result);
+        } else {
+            result.append(segment);
+        }
+    }
+
+    private boolean isVariable(String segment) {
+        return segment.startsWith("${") && segment.endsWith("}");
+    }
+
+    private void evaluateVariable(String segment, StringBuilder result) {
+        String var = segment.substring(2, segment.length() - 1);
+        if (!variables.containsKey(var)) {
             throw new MissingValueException("No value for " + segment);
         }
-        result.append(variables.get(var)); } else {
-        result.append(segment); }
+        result.append(variables.get(var));
     }
 
 }
