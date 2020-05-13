ec78ca8d99a2029915fc4ce67a0f80ccf3d87682
Pavlo Buidenkov <pavlobu@gmail.com>
2020-05-13 21:44:32 +0300

    refactor template

--- src/main/java/com/pavlobu/Template.java
+++ src/main/java/com/pavlobu/Template.java
@@ -21,14 +21,23 @@ public class Template {
     }
 
     public String evaluate() {
+        String result = replaceVariables();
+        checkForMissingValues(result);
+        return result;
+    }
+
+    private String replaceVariables() {
         String result = templateText;
         for (Map.Entry<String, String> entry : variables.entrySet()) {
             String regex = "\\$\\{" + entry.getKey() + "\\}";
             result = result.replaceAll(regex, entry.getValue());
         }
+        return result;
+    }
+
+    private void checkForMissingValues(String result) {
         if (result.matches(".*\\$\\{.+\\}.*")) {
             throw new MissingValueException();
         }
-        return result;
     }
 }
